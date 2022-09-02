1ce6405d50319eff938559e3751001c77e5f9002
Revathi G <revathikrishna0323@gmail.com>
2022-09-02 18:51:02 +0530

    properly formatted

--- src/main/java/com/jap/calculator/ScientificCalculator.java
+++ src/main/java/com/jap/calculator/ScientificCalculator.java
@@ -4,61 +4,57 @@ import static java.lang.Math.sqrt;
 
 public class ScientificCalculator {
 
-	public double cielOfANumber(float num) throws CalculatorException
-	{
-		if (num <= 0)
+    public static void main(String[] args) {
+        ScientificCalculator scientificCalculator = new ScientificCalculator();
+        try {
+            scientificCalculator.cielOfANumber(10);
+        } catch (CalculatorException e) {
+            throw new RuntimeException(e);
+        }
+        //Call all the methods one by one and surround them by try and respective catch blocks.
+    }
+
+    public double cielOfANumber(float num) throws CalculatorException {
+        if (num <= 0)
 
 
-			throw new CalculatorException();
+            throw new CalculatorException();
 
-		return Math.ceil(num);
+        return Math.ceil(num);
 
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-		//or else call ceil method of Math class and return the value
-
-
-	}
-
-	public double floorOfANumber(float num) throws CalculatorException {
-		if (num <= 0)
-
-			throw new CalculatorException();
-
-		return Math.floor(num);
-
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-		//or else call floor method of Math class and return the value
+        //check if num is equal to or less than 0 than throw the userdefined exception with proper message
+        //or else call ceil method of Math class and return the value
 
 
-	}
+    }
 
-	public long power(int num1, int num2) throws CalculatorException {
-		//if num1 is less than num2 than throw the userdefined exception with proper message
-		//else use Math pow method and return the value
+    public double floorOfANumber(float num) throws CalculatorException {
+        if (num <= 0)
 
-		if (num1 <= 0 || num2 <= 0)
-			throw new CalculatorException();
-		return (long) Math.pow(num1, num2);
-	}
+            throw new CalculatorException();
+
+        return Math.floor(num);
+
+        //check if num is equal to or less than 0 than throw the userdefined exception with proper message
+        //or else call floor method of Math class and return the value
 
 
-	public double squareRoot(int num) throws CalculatorException {
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-		//or else call sqrt method of Math class and return the value
+    }
 
-		if (num <= 0)
-			throw new CalculatorException();
+    public long power(int num1, int num2) throws CalculatorException {
+        //if num1 is less than num2 than throw the userdefined exception with proper message
+        //else use Math pow method and return the value
 
-		return (double) Math.sqrt(num);
-	}
+        if (num1 <= 0 || num2 <= 0) throw new CalculatorException();
+        return (long) Math.pow(num1, num2);
+    }
 
-	public static void main(String[] args) {
-		ScientificCalculator scientificCalculator = new ScientificCalculator();
-		try {
-			scientificCalculator.cielOfANumber(10);
-		} catch (CalculatorException e) {
-			throw new RuntimeException(e);
-		}
-		//Call all the methods one by one and surround them by try and respective catch blocks.
-	}
+    public double squareRoot(int num) throws CalculatorException {
+        //check if num is equal to or less than 0 than throw the userdefined exception with proper message
+        //or else call sqrt method of Math class and return the value
+
+        if (num <= 0) throw new CalculatorException();
+
+        return Math.sqrt(num);
+    }
 }
