6496a87ef3b67de67a9ea8a98348435bd1bf68aa
Revathi G <revathikrishna0323@gmail.com>
2022-09-02 18:50:32 +0530

    Added source code to ScientificCalculator.java

--- src/main/java/com/jap/calculator/ScientificCalculator.java
+++ src/main/java/com/jap/calculator/ScientificCalculator.java
@@ -1,36 +1,56 @@
 package com.jap.calculator;
 
+import static java.lang.Math.sqrt;
+
 public class ScientificCalculator {
 
 	public double cielOfANumber(float num) throws CalculatorException
 	{
+		if (num <= 0)
+
+
+			throw new CalculatorException();
+
+		return Math.ceil(num);
+
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
 		//or else call ceil method of Math class and return the value
 
-		return 1;
+
 	}
-	
-	public double floorOfANumber(float num) throws CalculatorException
-	{
+
+	public double floorOfANumber(float num) throws CalculatorException {
+		if (num <= 0)
+
+			throw new CalculatorException();
+
+		return Math.floor(num);
+
 		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
 		//or else call floor method of Math class and return the value
-		return 1;
+
+
 	}
-	
-    public long power(int num1, int num2) throws CalculatorException {
+
+	public long power(int num1, int num2) throws CalculatorException {
 		//if num1 is less than num2 than throw the userdefined exception with proper message
 		//else use Math pow method and return the value
 
-		return -1;
-    }
-    
-    public double squareRoot(int num) throws CalculatorException
-    {
-		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
-			//or else call sqrt method of Math class and return the value
+		if (num1 <= 0 || num2 <= 0)
+			throw new CalculatorException();
+		return (long) Math.pow(num1, num2);
+	}
 
-		return -1;
-    }
+
+	public double squareRoot(int num) throws CalculatorException {
+		//check if num is equal to or less than 0 than throw the userdefined exception with proper message
+		//or else call sqrt method of Math class and return the value
+
+		if (num <= 0)
+			throw new CalculatorException();
+
+		return (double) Math.sqrt(num);
+	}
 
 	public static void main(String[] args) {
 		ScientificCalculator scientificCalculator = new ScientificCalculator();
