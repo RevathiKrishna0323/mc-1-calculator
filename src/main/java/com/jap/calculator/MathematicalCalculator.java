988808e42a615355d6d351557665c1942139ba5a
Revathi G <revathikrishna0323@gmail.com>
2022-09-02 18:08:48 +0530

    Added source code to MathamaticalCalculator.java

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -2,26 +2,39 @@ package com.jap.calculator;
 
 public class MathematicalCalculator {
 
-	public int add(int num1, int num2)
-	{	
-		return -1;
-	}
-	public int subtract(int num1, int num2)
-	{	
-		return -2;
-	}
-	public int multiply(int num1, int num2)
-	{	
-		return -1;
-	}
-	public int divide(int num1, int num2)
-	{
-		//write the code and handle the ArithmeticException
-         return -1;
+    public int add(int num1, int num2) {
+        int add = num1 + num2;
+        return add;
+    }
 
-	}
-	public int modulo(int num1, int num2)
-	{	
-		return-1;
-	}
+    public int subtract(int num1, int num2) {
+        int subtract = num1 - num2;
+        return subtract;
+    }
+
+    public int multiply(int num1, int num2) {
+        int multiply = num1 * num2;
+        return multiply;
+    }
+
+    public int divide(int num1, int num2) {
+        //write the code and handle the ArithmeticException
+        int divide;
+        try {
+            divide = num1 / num2;
+        } catch (ArithmeticException exception) {
+            throw new RuntimeException(exception);
+        }
+        return divide;
+    }
+
+    public int modulo(int num1, int num2) {
+        int modulo = 0;
+        try {
+            modulo = num1 % num2;
+        } catch (ArithmeticException exception) {
+            throw new RuntimeException(exception);
+        }
+        return modulo;
+    }
 }
